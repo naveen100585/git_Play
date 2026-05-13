c0acbfc3a67ff7955dccc839f9097a12abdedbee
Naveen Kumar <naveenkumarrayapurail@gmail.com>
2026-05-13 17:34:33 +0530

    HHHHHGHGHSJGSGSJGDHFHDFHFD

--- sameeksha.spec.ts
+++ sameeksha.spec.ts
@@ -1,1 +1,14 @@
 I love Daughter too much addEventListener
+
+
+Commonly Used Command
+
+For first push:
+
+git push -u origin main
+
+Meaning:
+
+origin → remote repository name
+main → branch name
+-u → sets upstream tracking
